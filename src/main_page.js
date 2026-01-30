4026e6d88915fa7e09b7898d2aea65892467dd4c
Pavel25412 <lolkadoshirak@gmail.com>
2026-01-30 22:19:03 +0100

    initial

---
+++ src/main_page.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',() => {
+  const button = document.getElementById('cta-button');
+
+  button.addEventListener('click', () => {
+    alert('Скоро будет...');
+    console.log('Coming soon...');
+  });
+
+});
